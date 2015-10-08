bc9f8bbdc4b5dec2adac8b93d3a6e00dfda0d8a9
reznimic <mich.reznicek@gmail.com>
2015-10-08 13:21:31 +0200

    Preklady

--- sites/all/themes/koma/templates/node/node--certifikace.tpl.php
+++ sites/all/themes/koma/templates/node/node--certifikace.tpl.php
@@ -2,16 +2,13 @@
     <div class="row">
         <header class="m-section--header"></header>
     </div>
-
-    <?php
-    //    dsm($content);
-    //    dsm($node);
-    //    ?>
-
     <article class="m-reference">
         <header>
-            <div class="m-reference--image mm-preview-fit" style="background-image: url(<?= image_style_url('x618-0', $content['field_cert_nahled']['#items'][0]['uri']) ?>)">
-                <a href=""><img src="<?= image_style_url('x618-0', $content['field_cert_nahled']['#items'][0]['uri']) ?>" alt="" /></a>
+            <div class="m-reference--image mm-preview-fit"
+                 style="background-image: url(<?= image_style_url('x618-0', $content['field_cert_nahled']['#items'][0]['uri']) ?>)">
+                <a href="<?= image_style_url('x618-0', $content['field_cert_nahled']['#items'][0]['uri']) ?>"><img
+                        src="<?= image_style_url('x618-0', $content['field_cert_nahled']['#items'][0]['uri']) ?>"
+                        alt=""/></a>
             </div>
             <div class="m-reference--summary">
                 <h1 class="m-reference--hed"><a href=""><?php print $title; ?></a></h1>
@@ -23,15 +20,19 @@
                         <dd><?= ($content['field_cert_kategorie'][0]['#title']); ?></dd>
                     </dl>
                     <dl>
-                        <dt><?php print t('Certifikát ke stažení') ?></dt>
-                        <dd><a href="<?= (file_create_url($content['field_cert_cert']['#items'][0]['uri'])); ?>"><?= $title ?></a>
+                        <dt><?php print t('Download') ?></dt>
+                        <dd>
+                            <a href="<?= (file_create_url($content['field_cert_cert']['#items'][0]['uri'])); ?>"><?= $title ?></a>
                         </dd>
                     </dl>
-                    <dl>
-                        <dt><?php print t('Popis Certifikatu') ?></dt>
-                        <dd><?= $content['field_cert_popis']['#items'][0]['value'] ?></dd>
-                    </dl>
-
+                    <?php
+                    if (isset($content['field_cert_popis']['#items'][0]['value'])) {
+                        ?>
+                        <dl>
+                            <dt><?php print t('Description') ?></dt>
+                            <dd><?= $content['field_cert_popis']['#items'][0]['value'] ?></dd>
+                        </dl>
+                    <?php } ?>
                 </div>
             </div>
         </header>
@@ -44,7 +45,9 @@
             </div>
             <div class="l-half">
 
-                      <div class="m-section--more"><a href="http://www.koma-modular.cz/o-firme/certifikace"><?php print t('WHOLE ARCHIVES') ?> &rarr;</a></div>
+                <div class="m-section--more"><a
+                        href="http://www.koma-modular.cz/o-firme/certifikace"><?php print t('WHOLE ARCHIVES') ?> &rarr;</a>
+                </div>
             </div>
         </footer>
     </div>
