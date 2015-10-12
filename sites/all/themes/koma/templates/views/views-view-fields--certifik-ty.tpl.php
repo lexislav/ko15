847f7b7ee991956902382182bd4fcd2fc509c569
reznimic <mich.reznicek@gmail.com>
2015-10-12 15:59:52 +0200

    pridani odkazu

--- sites/all/themes/koma/templates/views/views-view-fields--certifik-ty.tpl.php
+++ sites/all/themes/koma/templates/views/views-view-fields--certifik-ty.tpl.php
@@ -16,8 +16,8 @@ if (isset($row->field_field_cert_nahled[0])) {
             </a>
         </div>
         <div class="m-item--summary">
-            <h1 class="m-item--hed">
-                <?= $row->_field_data['nid']['entity']->title ?>
+            <h1 class="m-item--hed"><a href="<?=test_lang_prefix('node/'.$row->nid)?>">
+                <?= $row->_field_data['nid']['entity']->title ?></a>
             </h1>
 
             <div class="m-item--description">
