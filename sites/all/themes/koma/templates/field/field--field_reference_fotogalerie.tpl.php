648a826703f8c92652c50834f2b512f7c8fe307a
reznimic <mich.reznicek@gmail.com>
2016-03-22 14:54:43 +0100

    fix hlavniho obrazku

--- sites/all/themes/koma/templates/field/field--field_reference_fotogalerie.tpl.php
+++ sites/all/themes/koma/templates/field/field--field_reference_fotogalerie.tpl.php
@@ -1,36 +1,74 @@
 <?php if (count($items) > 0) { ?>
 
-    <div class="m-section l-feed_four" <?php koma_theme_wrapper(__FILE__)?>>
-<!---->
-<!--        <div class="row">-->
-<!--            <header class="m-section--header mm-center">Photo gallery - rez: to tu nema co delat</header>-->
-<!--        </div>-->
+  <div class="m-section l-feed_four" <?php koma_theme_wrapper(__FILE__) ?>>
+    <!---->
+    <!--        <div class="row">-->
+    <!--            <header class="m-section--header mm-center">Photo gallery - rez: to tu nema co delat</header>-->
+    <!--        </div>-->
 
-        <div class="row rowfix">
+    <div class="row rowfix">
+      <?php
+      foreach ($items AS $pole) {
+
+        $cast = ($pole['node'][key($pole['node'])]['field_fotogalerie_imgs']['#items']);
+        ?>
+        <div class="l-single">
+          <ul class="m-gallery clearing-feature clearing-thumbs" data-clearing>
             <?php
-            foreach ($items AS $pole) {
-                $cast = ($pole['node'][key($pole['node'])]['field_fotogalerie_imgs']['#items']);
-                ?>
-                <div class="l-single">
-                    <ul class="m-gallery clearing-feature clearing-thumbs" data-clearing>
-                        <?php foreach ($cast AS $poradi => $img) {// prochazi jednotlive obrazky ?>
-                            <li class="m-gallery--item <?= ($poradi == 0) ? 'clearing-featured-img' : '' ?>">
-                                <article>
-                                    <a href="<?= image_style_url('none', $img['uri']) ?>"><img src="<?= image_style_url('x323-242', $img['uri']) ?>">
-                                        <h1 class="m-item--hed"><div class="wrapper"><i class="fa fa-picture-o"></i> <?=$pole['node'][key($pole['node'])]['#node']->title?></div></h1></a>
-                                </article>
-                            </li>
-                        <?php } ?>
-                    </ul>
-                </div>
+            $poradi = 0;
+            dpm($pole);
+            if (isset($pole['node'][key($pole['node'])]['field_fotogalerie_main_img']['#items'][0])) {
+              $poradi = 1;
+              $mainUri = $pole['node'][key($pole['node'])]['field_fotogalerie_main_img']['#items'][0]['uri'];
+              ?>
+              <li
+                class="m-gallery--item clearing-featured-img">
+                <article>
+                  <a href="<?= image_style_url('none', $mainUri) ?>"><img
+                      src="<?= image_style_url('x323-242', $mainUri) ?>">
 
-            <?php } ?>
+                    <h1 class="m-item--hed">
+                      <div class="wrapper"><i
+                          class="fa fa-picture-o"></i> <?= $pole['node'][key($pole['node'])]['#node']->title ?>
+                      </div>
+                    </h1>
+                  </a>
+                </article>
+              </li>
+              <?php
+            }
+            ?>
+            <?php
+
+
+            foreach ($cast AS $img) {// prochazi jednotlive obrazky ?>
+              <li
+                class="m-gallery--item <?= ($poradi == 0) ? 'clearing-featured-img' : '' ?>">
+                <article>
+                  <a href="<?= image_style_url('none', $img['uri']) ?>"><img
+                      src="<?= image_style_url('x323-242', $img['uri']) ?>">
+
+                    <h1 class="m-item--hed">
+                      <div class="wrapper"><i
+                          class="fa fa-picture-o"></i> <?= $pole['node'][key($pole['node'])]['#node']->title ?>
+                      </div>
+                    </h1>
+                  </a>
+                </article>
+              </li>
+            <?php
+              $poradi++;
+            } ?>
+          </ul>
         </div>
 
-        <div class="row">
-            <footer class="m-section--footer"></footer>
-        </div>
-
+      <?php } ?>
     </div>
+
+    <div class="row">
+      <footer class="m-section--footer"></footer>
+    </div>
+
+  </div>
 
 <?php } ?>
