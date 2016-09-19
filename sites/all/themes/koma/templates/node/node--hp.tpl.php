06ca4720f2e1d526a9ff75f0aacec476feb3da55
Michal Hotovec <michal.hotovec@gmail.com>
2016-09-19 13:23:08 +0200

    halfblock (+css)

--- sites/all/themes/koma/templates/node/node--hp.tpl.php
+++ sites/all/themes/koma/templates/node/node--hp.tpl.php
@@ -84,66 +84,6 @@ $pole_slider = $content['field_hlavni_slider']['#items'];
     </div>
 </div><!--koma zpravodaj-->
 
-<div class="m-section l-section_half-page">
-    <!-- left-->
-    <div class="l-half-wide mm-left mm-has-background" style="background-image: url('http://www.koma-modular.cz/sites/default/files/styles/x618-480/public/soubory/fotogalerie/koma-zpravodaj//uvodni/_mg_6997.jpg?itok=5TKNG8OR');">
-        <div class="l-content-wrap">
-            <div class="l-content">
-                <!-- obsah levé-->
-            </div>
-        </div>
-    </div>
-    <!-- right-->
-    <div class="l-half-wide mm-right bg-secondary">
-        <div class="l-content-wrap">
-            <div class="l-content">
-                <!-- obsah pravé-->
-
-                <div class="m-card m-card_half-block">
-                    <h2 class="m-card--headline mm-big">Headline</h2>
-                    <div class="m-card--content">
-                        <p>Modulární systém je rychlý, ekologický, šetrný k okolí, kvalitní, cenově výhodný, nákladově efektivní, flexibilní, mobilní.</p>
-                    </div>
-                    <div class="m-card--meta">
-                        <div class="button primary">Button →</div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="m-section l-section_half-page">
-    <!-- left-->
-    <div class="l-half-wide mm-left bg-white">
-        <div class="l-content-wrap">
-            <div class="l-content">
-                <!-- obsah pravé-->
-
-                <div class="m-card m-card_half-block">
-                    <h2 class="m-card--headline mm-big">Headline</h2>
-                    <div class="m-card--content">
-                        <p>Modulární systém je rychlý, ekologický, šetrný k okolí, kvalitní, cenově výhodný, nákladově efektivní, flexibilní, mobilní.</p>
-                    </div>
-                    <div class="m-card--meta">
-                        <div class="button primary">Button →</div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </div>
-    <!-- right-->
-    <div class="l-half-wide mm-right mm-has-background" style="background-image: url('http://www.koma-modular.cz/sites/default/files/styles/x618-480/public/soubory/fotogalerie/koma-zpravodaj//uvodni/_mg_6997.jpg?itok=5TKNG8OR');">
-        <div class="l-content-wrap">
-            <div class="l-content">
-                <!-- obsah levé-->
-            </div>
-        </div>
-    </div>
-</div>
-
 
 <div class="m-section l-feed_block-carousel bg-white" <?php koma_theme_wrapper(__FILE__)?>>
     <div class="m-section--underlay"></div>
