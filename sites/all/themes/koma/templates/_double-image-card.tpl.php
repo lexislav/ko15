ea2689e0468a5bb4731e66df460d98f5955091e5
Michal Hotovec <michal.hotovec@gmail.com>
2016-10-05 11:15:59 +0200

    Double image card

---
+++ sites/all/themes/koma/templates/_double-image-card.tpl.php
@@ -0,0 +1,69 @@
+<div class="m-section l-feed_two" theme-wrapper="_double-image-card.tpl.php">
+
+    <!-- section header-->
+    <div class="row">
+        <header class="m-section--header">
+            <div class="l-full">
+            </div>
+        </header>
+    </div>
+
+    <div class="row rowfix">
+
+        <div class="m-card_image l-single">
+
+            <article class="m-story">
+                <header>
+                    <div class="m-item--image" style="background-image: url(http://www.koma-modular.cz/sites/default/files/styles/x595-0/public/segment_trhu/skolske-stavby-deti-uhrnieves.jpg?itok=gL4k7ZDb)">
+                        <a href="http://www.koma-modular.cz/realizace-skolske">
+                            <img src="http://www.koma-modular.cz/sites/default/files/styles/x595-0/public/segment_trhu/skolske-stavby-deti-uhrnieves.jpg?itok=gL4k7ZDb" alt="">
+                        </a>
+                    </div>
+                    <div class="m-item--summary">
+                        <h1 class="m-item--hed">
+                            <a href="http://www.koma-modular.cz/realizace-skolske">Realizace školské</a>
+                        </h1>
+
+
+                    </div>
+                </header>
+                <div class="m-card--more">→ </div>
+            </article>
+
+        </div>
+
+
+        <div class="m-card_image l-single">
+
+            <article class="m-story">
+                <header>
+                    <div class="m-item--image" style="background-image: url(http://www.koma-modular.cz/sites/default/files/styles/x595-0/public/segment_trhu/img_2256.jpg?itok=GqFmlmf_)">
+                        <a href="http://www.koma-modular.cz/realizace-pro-podnikani">
+                            <img src="http://www.koma-modular.cz/sites/default/files/styles/x595-0/public/segment_trhu/img_2256.jpg?itok=GqFmlmf_" alt="">
+                        </a>
+                    </div>
+                    <div class="m-item--summary">
+                        <h1 class="m-item--hed">
+                            <a href="http://www.koma-modular.cz/realizace-pro-podnikani">Realizace pro podnikání</a>
+                        </h1>
+
+
+                    </div>
+                </header>
+                <div class="m-card--more mm-white">→ </div>
+            </article>
+
+        </div>
+
+    </div>
+
+    <!-- section footer -->
+    <div class="row">
+        <footer class="m-section--footer">
+            <div class="l-half">
+            </div>
+            <div class="l-half">
+            </div>
+        </footer>
+    </div>
+</div>
