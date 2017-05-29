a5120f0daee78e62bc03672e0a38770f803008eb
reznicekm <mich.reznicek@gmail.com>
2017-05-29 18:03:16 +0200

    lexovo zmeny

--- sites/all/themes/koma/templates/node/node--2665.tpl.php
+++ sites/all/themes/koma/templates/node/node--2665.tpl.php
@@ -4,37 +4,28 @@ $body = field_view_value('node', $node, 'body', $body_field[0]);
 $img_path = image_style_url('x618-480', $node->field_jeden_obrazek['und'][0]['uri'])
 ?>
 <div class="m-section" theme-wrapper="node--pronajem_kontejneru.tpl.php">
+    
 
-    <!-- header sekce-->
-    <div class="row">
-        <header class="m-section--header"></header>
-    </div>
-
-    <!-- artikl -->
-
-    <article>
-        <div class="row">
-            <!-- 1/2-->
-            <div class="l-half">
-                <header>
-                    <h1 class="m-item--hed mm-bold mm-big mm-color-black"><?=$title?></h1>
-                </header>
-                <div class="m-item--description mm-pad-bottom">
-                  <?php print render($body);?>
-                </div>
-
+    <div class="row mm-pad-bottom">
+        <header class="m-section--header">
+            <div class="l-full">
+              <h2 class="m-section--hed mm-big"><?=$title?></h2>
             </div>
-            <!-- 2/2-->
-            <div class="l-half">
-                <div class="m-item--image" style="background-image: url(<?=$img_path?>)">
-                    <img src="<?=$img_path?>" alt="">
-                </div>
+        </header>
+
+        <!-- 1/1-->    
+        <div class="l-half">
+            <div class="m-item--description mm-pad-bottom">
+                <?php print render($body);?>
             </div>
         </div>
-    </article>
-
-    <div class="row">
-        <footer class="m-section--footer"></footer>
+        
+        <!-- 2/2-->
+        <div class="small-12 large-offset-1 large-5 columns">
+            <div class="m-item--image" style="background-image: url(<?=$img_path?>)">
+                <img src="<?=$img_path?>" alt="">
+            </div>
+        </div>
     </div>
 
 </div>
