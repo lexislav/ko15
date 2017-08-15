b5f99a3d9d98ea3f6c5e194f5dff4921fd07addc
Michal Hotovec <michal.hotovec@gmail.com>
2017-08-15 16:06:25 +0200

    reformat

--- sites/all/themes/koma/templates/tb/tb-megamenu-block.tpl.php
+++ sites/all/themes/koma/templates/tb/tb-megamenu-block.tpl.php
@@ -1,8 +1,6 @@
 <?php if ($content) : ?>
     <div class="tblock">
         <?php
-
-
         $content = str_replace('</table></p>','</table>',$content);
         print str_replace('<p><h3>','<h3>',$content) ?>
     </div>
