da707668df631242236b04465028832e5076a81b
Michal Hotovec <michal.hotovec@gmail.com>
2018-09-26 17:14:03 +0200

    JS na odeslání GA

--- sites/all/themes/koma/js/script.js
+++ sites/all/themes/koma/js/script.js
@@ -1,6 +1,31 @@
 jQuery(document).ready(function($) {
-// Track submission events.
+  function isEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+  }
+
+  // Track submission events.
   $('#webform-client-form-1602').submit(function() {
-    console.log('neco nekde nejak nekam');
+
+    var senderr = false;
+    // test errors
+    if (!($('#edit-submitted-souhlasim-1').is(":checked"))) {
+      console.log('no gdpr');
+      senderr =  true;
+    }
+    if (!isEmail($('#edit-submitted-e-mail').val())) {
+      console.log("email err");
+      senderr =  true;
+    }
+
+    if(!senderr) {
+      if(ga) {
+         ga('send', 'event', 'button', 'click', 'poslat_poptavku');
+
+      } else {
+        console.log('missing ga');
+      }
+    }
+
   });
 });
