f583e5ce41963de6ecdf4e3414b311ed6e229397
Alexandr Hudeček <lexislav@centrum.cz>
2018-10-11 23:11:44 +0200

    adjusting html

--- sites/all/themes/koma/templates/views/views-view-fields--blok-kontaktu.tpl.php
+++ sites/all/themes/koma/templates/views/views-view-fields--blok-kontaktu.tpl.php
@@ -1,11 +1,13 @@
-<div class="okolokontaktu">
+<div class="m-contact-tab--address">
+  <div class="m-address">
+    <div class="m-address--name"><?php print $fields['nothing']->content; //name ?></div>
+    <div class="m-address--job">
+      <div class="field-content"><?php print $fields['field_kontakt_pozice']->content; ?></div>
+    </div>
 
-  <?php print $fields['nothing']->content; //name ?>
-  <?php print $fields['sitename']->content; ?>
-  <?php print $fields['field_kontakt_pozice']->content; ?>
-  <?php print $fields['kontakt_telefon']->content; ?>
-  <?php print $fields['field_kontakt_pevna']->content; ?>
-  <?php print $fields['kontakt_fax']->content; ?>
-  <?php print $fields['kontakt_email']->content; ?>
-
+    <div class="m-address--phone"><span>T</span> <?php print $fields['field_kontakt_pevna']->content; ?></div>
+    <div class="m-address--phone"><span>M</span> <?php print $fields['kontakt_telefon']->content; ?></div>
+    <?php if (!empty($fields['kontakt_fax']->content)): ?><div class="m-address--fax"><span>F</span> <?php print $fields['kontakt_fax']->content; ?></div><?php endif ?>
+    <div class="m-address--mail"><span>E</span> <a href='mailto:<?php print $fields['kontakt_email']->content; ?>'><?php print $fields['kontakt_email']->content; ?></a></div>
+  </div>
 </div>
