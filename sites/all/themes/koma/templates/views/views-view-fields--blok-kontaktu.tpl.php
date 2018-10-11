f980a6b07b38f38c32359fd1ca816a09aff99538
reznicekm <mich.reznicek@gmail.com>
2018-10-11 15:57:16 +0200

    Add tpl for contacts

---
+++ sites/all/themes/koma/templates/views/views-view-fields--blok-kontaktu.tpl.php
@@ -0,0 +1,11 @@
+<div class="okolokontaktu">
+
+  <?php print $fields['nothing']->content; //name ?>
+  <?php print $fields['sitename']->content; ?>
+  <?php print $fields['field_kontakt_pozice']->content; ?>
+  <?php print $fields['kontakt_telefon']->content; ?>
+  <?php print $fields['field_kontakt_pevna']->content; ?>
+  <?php print $fields['kontakt_fax']->content; ?>
+  <?php print $fields['kontakt_email']->content; ?>
+
+</div>
