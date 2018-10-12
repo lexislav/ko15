c81468021ab1723b58e8322c005a57d8a5a47675
Alexandr Hudeček <lexislav@centrum.cz>
2018-10-12 14:51:06 +0200

    COnditionals for output content in telefonni seznam

--- sites/all/themes/koma/templates/views/views-view-fields--blok-kontaktu.tpl.php
+++ sites/all/themes/koma/templates/views/views-view-fields--blok-kontaktu.tpl.php
@@ -4,16 +4,18 @@
     <div class="m-address--job">
       <div class="field-content">
         <?php print $fields['field_kontakt_pozice']->content; ?><br>
-        <?php print $fields['sitename']->content; ?>
-        <?php if ($fields['sitename']->content === "www.koma-modular.cz"): ?>els hop
-        <?php endif; ?>
+        <?php if ($fields['sitename']->content === '<span>koma-slovakia.sk</span>'): ?>KOMA SLOVAKIA<?php endif; ?>
+        <?php if ($fields['sitename']->content === '<span>www.koma-modular.cz</span>'): ?>KOMA MODULAR<?php endif; ?>
+        <?php if ($fields['sitename']->content === '<span>www.koma-facade.cz</span>'): ?>KOMA FACADE<?php endif; ?>
+        <?php if ($fields['sitename']->content === '<span>www.koma-rent.cz</span>'): ?>KOMA RENT<?php endif; ?>
+        <?php if ($fields['sitename']->content === '<span>www.koma-space.at</span>'): ?>KOMA SPACE<?php endif; ?>
       </div>
       
     </div>
 
-    <div class="m-address--phone"><span>T</span> <?php print $fields['field_kontakt_pevna']->content; ?></div>
-    <div class="m-address--phone"><span>M</span> <?php print $fields['kontakt_telefon']->content; ?></div>
-    <?php if (!empty($fields['kontakt_fax']->content)): ?><div class="m-address--fax"><span>F</span> <?php print $fields['kontakt_fax']->content; ?></div><?php endif ?>
-    <div class="m-address--mail"><span>E</span> <a href='mailto:<?php print $fields['kontakt_email']->content; ?>'><?php print $fields['kontakt_email']->content; ?></a></div>
+    <?php if ($fields['field_kontakt_pevna']->content <> '<div class="field-content"></div>'): ?><div class="m-address--phone"><span>T</span> <?php print $fields['field_kontakt_pevna']->content; ?></div><?php endif ?>
+    <?php if ($fields['kontakt_telefon']->content <> '<div class="field-content"></div>'): ?><div class="m-address--phone"><span>M</span> <?php print $fields['kontakt_telefon']->content; ?></div><?php endif ?>
+    <?php if ($fields['kontakt_fax']->content <> '<div class="field-content"></div>'): ?><div class="m-address--fax"><span>F</span> <?php print $fields['kontakt_fax']->content; ?></div><?php endif ?>
+    <?php if ($fields['kontakt_email']->content <> '<div class="field-content"></div>'): ?><div class="m-address--mail"><span>E</span> <a href='mailto:<?php print $fields['kontakt_email']->content; ?>'><?php print $fields['kontakt_email']->content; ?></a></div><?php endif ?>
   </div>
 </div>
