fb13b7961d699e6446721b26f26b2f1334a461ca
Alexandr Hudeček <lexislav@centrum.cz>
2018-10-15 16:50:36 +0200

    header format and tests

--- sites/all/themes/koma/templates/views/views-view--blok-kontaktu--page.tpl.php
+++ sites/all/themes/koma/templates/views/views-view--blok-kontaktu--page.tpl.php
@@ -3,78 +3,89 @@
 <?php endif; ?>
 <div class="m-section <?= $trida ?>">
 
-    <div class="row">
-        <header class="m-section--header">
+    <header class="m-section--header">
+      <div class="row ">
+        <div class="l-full">
 
-            <nav class="breadcrumbs">
+          <nav class="breadcrumbs">
+              <a href="/">Domů</a><a href="/kontakt">Kontakt</a><a href="/kontakt/telefonni-seznam">Telefonní seznam firmy</a> -->
               <?php
-              foreach (drupal_get_breadcrumb() AS $drobek) {
-                echo $drobek;
-              }
-              ?>
-            </nav>
+                foreach (drupal_get_breadcrumb() AS $drobek) {
+                  echo $drobek;
+                }
+                ?>
+          </nav>
 
+        </div>
+      </div>
+
+      <div class="row">
+      
             <div class="l-full">
                 <h2 class="m-section--hed mm-big"><?php print t($title); ?></h2>
             </div>
-        </header>
-    </div>
-
-
-  <?php if ($header): ?>
-      <div class="view-header">
-        <?php print $header; ?>
+      
       </div>
-  <?php endif; ?>
+    </header>
 
-  <?php if ($exposed): ?>
-      <div class="view-filters">
-        <?php print $exposed; ?>
-      </div>
-  <?php endif; ?>
+  </div>
 
-  <?php if ($attachment_before): ?>
-      <div class="attachment attachment-before">
-        <?php print $attachment_before; ?>
-      </div>
-  <?php endif; ?>
 
-  <?php if ($rows): ?>
-      <div class="view-content">
-        <?php print $rows; ?>
-      </div>
-  <?php elseif ($empty): ?>
-      <div class="view-empty">
-        <?php print $empty; ?>
-      </div>
-  <?php endif; ?>
+    <?php if ($header): ?>
+        <div class="view-header">
+          <?php print $header; ?>
+        </div>
+    <?php endif; ?>
 
-  <?php if ($pager): ?>
-    <?php print $pager; ?>
-  <?php endif; ?>
+    <?php if ($exposed): ?>
+        <div class="view-filters">
+          <?php print $exposed; ?>
+        </div>
+    <?php endif; ?>
 
-  <?php if ($attachment_after): ?>
-      <div class="attachment attachment-after">
-        <?php print $attachment_after; ?>
-      </div>
-  <?php endif; ?>
+    <?php if ($attachment_before): ?>
+        <div class="attachment attachment-before">
+          <?php print $attachment_before; ?>
+        </div>
+    <?php endif; ?>
 
-  <?php if ($more): ?>
-    <?php print $more; ?>
-  <?php endif; ?>
+    <?php if ($rows): ?>
+        <div class="m-section bg-white">
+          <div class="row">
+          <?php print $rows; ?>
+          </div>
+        </div>
+    <?php elseif ($empty): ?>
+        <div class="view-empty">
+          <?php print $empty; ?>
+        </div>
+    <?php endif; ?>
 
-  <?php if ($footer): ?>
-      <div class="view-footer">
-        <?php print $footer; ?>
-      </div>
-  <?php endif; ?>
+    <?php if ($pager): ?>
+      <?php print $pager; ?>
+    <?php endif; ?>
 
-  <?php if ($feed_icon): ?>
-      <div class="feed-icon">
-        <?php print $feed_icon; ?>
-      </div>
-  <?php endif; ?>
+    <?php if ($attachment_after): ?>
+        <div class="attachment attachment-after">
+          <?php print $attachment_after; ?>
+        </div>
+    <?php endif; ?>
 
+    <?php if ($more): ?>
+      <?php print $more; ?>
+    <?php endif; ?>
+
+    <?php if ($footer): ?>
+        <div class="view-footer">
+          <?php print $footer; ?>
+        </div>
+    <?php endif; ?>
+
+    <?php if ($feed_icon): ?>
+        <div class="feed-icon">
+          <?php print $feed_icon; ?>
+        </div>
+    <?php endif; ?>
 </div><?php /* class view */ ?>
 
 
--- sites/all/themes/koma/templates/views/views-view-unformatted--blok-kontaktu.tpl.php
+++ sites/all/themes/koma/templates/views/views-view-unformatted--blok-kontaktu.tpl.php
@@ -1,27 +1,36 @@
-<div class="m-contact-list">
-    <style type="text/css">
-        .field-content, .field-content span {
-            display: inline-block;
-            width: auto;
-        }
+<div class="row">
+    <header class="m-section--header">
+        <div class="l-full">
 
-        .m-contact-tab--content .m-contact-tab--address .m-address--name span {
-            width: 100%;
-        }
-    </style>
-
-    <div class="row">
-
-<?php if (!empty($title)): ?>
-    <h3><?php print $title; ?></h3>
-    <hr>
-<?php endif; ?>
-
-<div class="m-contact-tab--content "> 
-<?php foreach ($rows as $id => $row): ?>
-      <?php print $row; ?>
-<?php endforeach; ?>
+        </div>
+    </header>
 </div>
+<div class="row">
+    <div class="m-contact-list">
+        <style type="text/css">
+            .field-content, .field-content span {
+                display: inline-block;
+                width: auto;
+            }
 
-</div>
+            .m-contact-tab--content .m-contact-tab--address .m-address--name span {
+                width: 100%;
+            }
+        </style>
+
+        <div class="row">
+
+            <?php if (!empty($title)): ?>
+                <h3><?php print $title; ?></h3>
+                <hr>
+            <?php endif; ?>
+
+            <div class="m-contact-tab--content "> 
+                <?php foreach ($rows as $id => $row): ?>
+                    <?php print $row; ?>
+                <?php endforeach; ?>
+            </div>
+
+        </div>
+    </div>
 </div>
