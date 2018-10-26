d25eed647f6da5f1d96d1751d32f985b75c38b43
reznicekm <mich.reznicek@gmail.com>
2018-10-26 11:12:05 +0200

    Add template for new contact

---
+++ sites/all/themes/koma_facade/templates/node/node--3097.tpl.php
@@ -0,0 +1,373 @@
+<style>
+
+    .m-contact-form.mm-full {
+        width: 100%;
+    }
+
+    .m-contact.mm-contanct-override {}
+    .m-contact.mm-contanct-override a {
+        color: black;
+        text-decoration: none;
+    }
+    .m-contact.mm-contanct-override a:hover {
+        color: rgb(194, 14, 26);
+        text-decoration: underline;
+    }
+    .m-contact.mm-contanct-override .m-contact--summary {
+        width: 50%;
+        margin: 0;
+        padding-right: 30px;
+    }
+
+
+
+    @media (max-width: 960px) {
+        .l-half.l-half-mobile-full {
+            width: calc(100% * 1 - 30px - 1px);
+        }
+    }
+
+    @media (min-width: 961px) {
+        .pt-0 {
+            padding-top: 0 !important;
+        }
+    }
+</style>
+
+
+<div class="m-section l-detail-page" <?php koma_theme_wrapper(__FILE__) ?>>
+    <div class="row">
+        <header class="m-section--header">
+            <div class="l-half">
+                <h2 class="m-section--hed mm-medium"><?= $node->title ?></h2>
+            </div>
+            <div class="l-half">
+                <ul class="m-section--nav inline-right">
+                    <li><a href="https://www.facebook.com/KOMAmodular" target="" title=""><i class="fa fa-facebook"></i></a>
+                    </li>
+                    <li><a href="https://www.linkedin.com/company/2810844/" target="" title=""><i class="fa fa-linkedin"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://www.youtube.com/user/TopKOMASPACE" target="" title=""><i class="fa fa-youtube-play"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </header>
+    </div>
+
+    <article class="m-contact mm-contanct-override">
+        <header>
+
+            <div class="l-half l-half-mobile-full">
+                <!-- prvni adresa-->
+                <div class="m-contact--summary">
+                    <h1 class="m-contact--hed color-black"><?= $content['field_basic_title']['#items'][0]['value'] ?></h1>
+                    <div class="m-contact--description">
+                        <?= $content['field_basic_text']['#items'][0]['value'] ?>
+                    </div>
+                    <div class="m-contact-mapa">
+                        <br><i class="fa fa-globe"></i> <a href="<?= $content['field_page_link']['#items'][0]['url'] ?>" target="_blank" title="<?= $content['field_page_link']['#items'][0]['title'] ?>"><?= $content['field_page_link']['#items'][0]['title'] ?></a><br>
+                    </div>
+                    <div class="m-contact--meta">
+                        <?php if ($content['kontakt_telefon']['#items'][0]['value']): ?>
+                            <br />
+                            <span class="color-primary">T</span> <?= $content['kontakt_telefon']['#items'][0]['value'] ?>
+                            <br /><?php endif ?>
+                        <?php if ($content['kontakt_fax']['#items'][0]['value']): ?>
+                            <span class="color-primary">F</span> <?= $content['kontakt_fax']['#items'][0]['value'] ?>
+                            <br /><?php endif ?>
+                        <?php if ($content['kontakt_email']['#items'][0]['value']): ?>
+                            <span class="color-primary">E</span><a href="mailto:<?= $content['kontakt_email']['#items'][0]['value'] ?>"> <?= $content['kontakt_email']['#items'][0]['value'] ?></a></span>
+                            <br /><?php endif ?>
+                    </div>
+                </div>
+                <!-- druha adresa (if exists) -->
+                <?php if ($content['field_reference_text']['#items'][0]['value']): ?>
+                    <div class="m-contact--summary">
+                        <h1 class="m-contact--hed color-black"><?= $content['field_firma_adresa_2']['#items'][0]['value'] ?></h1>
+
+                        <div class="m-contact--description">
+
+                            <?= ($content['field_reference_text']['#items'][0]['value']) ? $content['field_reference_text']['#items'][0]['value'] : '' ?>
+
+                        </div>
+
+                        <div class="m-contact-mapa">
+                            <br><i class="fa fa-globe"></i> <a href="<?= $content['field_odkazy_p_link']['#items'][0]['url'] ?>" target="_blank" title="<?= $content['field_odkazy_p_link']['#items'][0]['title'] ?>"><?= $content['field_odkazy_p_link']['#items'][0]['title'] ?></a><br>
+                        </div>
+
+                        <div class="m-contact--meta">
+                            <?php if ($content['field_telefon_2']['#items'][0]['value']): ?>
+                                <br />
+                                <span class="color-primary">T</span> <?= $content['field_telefon_2']['#items'][0]['value'] ?>
+                                <br /><?php endif ?>
+                            <?php if ($content['field_fax_2']['#items'][0]['value']): ?>
+                                <span class="color-primary">F</span> <?= $content['field_fax_2']['#items'][0]['value'] ?>
+                                <br /><?php endif ?>
+                            <?php if ($content['field_email_2']['#items'][0]['value']): ?>
+                                <span class="color-primary">E</span><a href="mailto:<?= $content['field_email_2']['#items'][0]['value'] ?>"> <?= $content['field_email_2']['#items'][0]['value'] ?></a></span>
+                                <br /><?php endif ?>
+                        </div>
+                    </div>
+                <?php endif ?>
+            </div>
+
+            <div class="l-half l-half-mobile-full">
+                <?php
+
+                if (isset($_POST['mailto'])) {
+                if (empty($_POST['lastname'])) {
+                    $mailto = $_POST['mailto'];
+                    $mailfrom = $_POST['email'];
+                    $name = $_POST['name'];
+                    $text = $_POST['body'];
+                    test_mail($mailfrom, $mailto, 'Nová zpráva - kontaktní formulář KOMA', $name . "\n" . $text);
+                    test_mail('info@koma-modular.cz', $mailfrom, 'Nová zpráva - kontaktní formulář KOMA', $name . "\n" . $text);
+
+                    ?>
+                    <div class="m-section" id="contact-form" <?php koma_theme_wrapper(__FILE__) ?>>
+                        <div class="row">
+                            <header class="m-section--header">
+                                <div class="l-full">
+                                    <h2 class="m-section--hed mm-tiny mm-medium mm-center"><?php print t('Formulář byl úspěšně odeslán') ?></h2>
+                                </div>
+                            </header>
+                        </div>
+
+                        <div class="row">
+                            <div class="m-contact-form mm-sent">
+                                <div class="m-contact-form--content">
+                                    <p>
+                                        <i class="fa fa-paper-plane-o"></i> <?php print t('Formulář byl úspěšně odeslán na adresu') ?>
+                                        :
+                                        <span><?= $mailto ?></span>
+                                        <br /><?php print t('a na vámi zadanou adresu') ?>
+                                        <span><?= $mailfrom ?></span>
+                                    </p>
+                                    <a href="http://<?= $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] ?>" class="button">Odeslat novou zprávu
+                                        <i class="fa fa-paper-plane-o"></i></a>
+                                    <br />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!--  Metrics start  -->
+                    <!-- Google Code for Odesl&aacute;n&iacute; kontaktn&iacute;ho formul&aacute;&#345;e Conversion Page -->
+                    <script type="text/javascript">
+                      /* <![CDATA[ */
+                      var google_conversion_id = 847326048;
+                      var google_conversion_language = "en";
+                      var google_conversion_format = "3";
+                      var google_conversion_color = "ffffff";
+                      var google_conversion_label = "x9y2CPqRy3IQ4NaElAM";
+                      var google_remarketing_only = false;
+                      /* ]]> */
+                    </script>
+                    <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js"></script>
+                    <noscript>
+                        <div style="display:inline;">
+                            <img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/847326048/?label=x9y2CPqRy3IQ4NaElAM&amp;guid=ON&amp;script=0" />
+                        </div>
+                    </noscript>
+
+                    <!-- Měřicí kód Sklik.cz -->
+                    <iframe width="119" height="22" frameborder="0" scrolling="no" src="//c.imedia.cz/checkConversion?c=100033707&amp;color=ffffff&amp;v=0"></iframe>
+
+                    <script>
+
+                      jQuery(document).ready(function () {
+                        // ga
+                        if (isGAApi(2018)) {
+                          gtag('event', 'send', {
+                            'event_category': 'form',
+                            'event_action': 'sent',
+                            'event_label': 'odeslat zpravu'
+                          });
+                        }
+                        else {
+                          ga('send', 'event', 'form', 'sent', 'odeslat zpravu');
+                        }
+
+                        // FB
+                        fbq('track', '<FORMULAR_SEND>');
+
+                      });
+                    </script><!--  Metrics: End  -->
+
+                <?php }
+                } else { ?>
+
+                    <div class="m-section" id="formSection" <?php koma_theme_wrapper(__FILE__) ?>>
+                        <div class="row">
+                            <header class="m-section--header pt-0">
+                                <div class="l-full">
+                                    <h2 class="m-section--hed mm-tiny mm-medium mm-center"><?php print t('DO YOU WANT TO ASK ABOUT ANYTHING?') ?>
+                                        <span class="color-primary"> <?php print t('WRITE TO US') ?></span>
+                                    </h2>
+                                </div>
+                            </header>
+                        </div>
+
+                        <div class="row">
+                            <div class="m-contact-form mm-full">
+                                <header>
+                                    <i class="fa fa-paper-plane-o"></i> <?php print t('The contact form will be sent to the following address') ?>
+                                    :
+                                    <span id="targetMailInfo">info@koma-modular.cz</span>
+                                </header>
+
+
+                                <!--@TODO dodelat cele vsechno omg-->
+
+                                <div class="m-contact-form--content">
+                                    <form action="<?php print url('node/3074', ['absolute' => true]); ?>#contact-form" method="post">
+                                        <input type="hidden" name="mailto" id="mailto" value="info@koma-modular.cz" />
+                                        <input type="text" name="lastname" class="lastname" value="" />
+                                        <select name="option">
+                                            <?php foreach ($content['field_dropdown']['#items'] as $item) {
+                                                ?>
+                                                <option value="<?= $item['value'] ?>"><?= $item['value'] ?></option>
+                                                <?php
+                                            }
+                                            ?>
+                                        </select>
+                                        <input type="text" id="name" name="name" placeholder="<?php print t('Your first and surname') ?>" required />
+                                        <input type="email" id="email" name="email" placeholder="<?php print t('Your e-mail address') ?>" required />
+                                        <textarea id="body" name="body" style="height: 200px" required placeholder="<?php print t('The text of your question') ?>"></textarea>
+                                        <button type="submit" class="button"><?php print t('Send the message') ?>
+                                            <i class="fa fa-paper-plane-o"></i></button>
+                                    </form>
+                                </div>
+
+
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                    <?php
+                }
+                ?>
+
+            </div>
+
+        </header>
+    </article>
+
+    <div class="row">
+        <footer class="m-section--footer">
+            <div class="l-half">
+                <a href="https://www.koma-modular.cz/kontakt/telefonni-seznam" onclick="ga('send', 'event', 'button', 'click', 'adresar');">
+                    <button class="button">Telefonní seznam firmy</button>
+                </a>
+            </div>
+            <div class="l-half">
+                <ul class="m-section--nav inline-right">
+                    <!-- @todo: add measure code-->
+                    <li>
+                        <a href="#kariera" onclick="ga('send', 'event', 'button', 'click', 'kariera v koma');"><?php print t('Career with KOMA') ?> </a>
+                    </li>
+                    <li><a href="/kontakt/telefonni-seznam">&darr; <?php print t('Directory') ?> </a></li>
+                    <?php /*<li>
+                        <a href="#formSection" onclick="ga('send', 'event', 'button', 'click', 'tlacitko kontaktni formular');">&darr; <?php print t('Contact form') ?> </a>
+                    </li>*/ ?>
+                </ul>
+            </div>
+        </footer>
+    </div>
+
+</div>
+
+<?php
+/*
+<!-- contact list -->
+<div class="m-section bg-white" id="adresar" <?php koma_theme_wrapper(__FILE__) ?>>
+    <div class="row">
+        <header class="m-section--header">
+            <div class="l-half">
+                <h2 class="m-section--hed mm-small color-primary"><?php print t('COMPANY DIRECTORY') ?></h2>
+            </div>
+        </header>
+    </div>
+    <?php
+    $block = module_invoke('views', 'block_view', 'kontakty-block');
+    print render($block);
+    ?>
+
+    <div class="row">
+        <footer class="m-section--footer"></footer>
+    </div>
+</div>
+*/
+?>
+
+
+
+<!-- volna mista-->
+<div class="m-section bg-white" id="kariera" <?php koma_theme_wrapper(__FILE__) ?>>
+
+    <?php
+    global $language;
+    if ($language->language == 'cs') {
+        ?>
+        <div class="row">
+            <header class="m-section--header"></header>
+        </div>
+        <div class="row">
+            <div class="m-card_bigpromo l-single bg-secondary-light">
+
+                <article class="m-story">
+                    <header>
+                        <div class="m-item--image" style="background-image: url(<?= image_style_url('x633-540', $content['field_basic_img']['#items'][0]['uri']) ?>)">
+                            <a href="<?= test_lang_prefix('firma-a-lide/kariera') ?>">
+                                <img src="<?= image_style_url('x633-540', $content['field_basic_img']['#items'][0]['uri']) ?>" alt="">
+                            </a>
+                        </div>
+                        <div class="m-item--summary">
+                            <h1 class="m-item--hed mm-center">
+                                <a href="<?= test_lang_prefix('firma-a-lide/kariera') ?>" onclick="ga('send', 'event', 'button', 'click', 'kariera v koma');"><?php print t('Kariéra v KOMA') ?></a>
+                            </h1>
+
+                            <div class="m-item--description">
+                                <?= $content['field_basic_popis_2']['#items'][0]['value'] ?>
+                            </div>
+
+                            <div class="m-card--more bg-8">
+                                <a href="<?= test_lang_prefix('firma-a-lide/kariera') ?>" title="<?php print t('Aktuální volná místa') ?>" onclick="ga('send', 'event', 'button', 'click', 'aktualni volna mista');"><?php print t('Aktuální volná místa') ?>&nbsp;→</a>
+                            </div>
+                        </div>
+                    </header>
+
+                </article>
+
+            </div>
+        </div>
+        <?php
+    }
+    ?>
+    <div class="row">
+        <footer class="m-section--footer">
+            <div class="l-half">
+                <div class="m-section--top"><a href=""><?php print t('UP') ?> &uarr;</a>
+                </div>
+            </div>
+            <div class="l-half">
+                <ul class="m-section--nav inline-right">
+                    <li>
+                        <a href="<?= test_lang_prefix('node/1552 ') ?>"><?php print t('Company & people') ?></a>
+                    </li>
+                    <li>
+                        <a href="<?= test_lang_prefix('node/1612') ?>"><?php print t('HODNOTY, FILOZOFIE, VIZE') ?> </a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    </div>
+</div>
+
+
+<!--@TODO nacitat drupaloidne-->
+<script type="text/javascript" src="<?= $GLOBALS['base_url'] ?>/sites/all/themes/koma/js/contact.js"></script>
+
+
